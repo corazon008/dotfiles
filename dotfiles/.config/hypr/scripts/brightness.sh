3ce3735072e26aee1165641336587996c7601cea
corazon <99136079+corazon008@users.noreply.github.com>
2026-06-23 12:03:05 +0200

    Update brightness.sh

--- dotfiles/.config/hypr/scripts/brightness.sh
+++ dotfiles/.config/hypr/scripts/brightness.sh
@@ -1,20 +1,76 @@
 #!/usr/bin/env bash
 
-delta="$1"
+get_mode() {
+    if ls /sys/class/backlight/* >/dev/null 2>&1; then
+        echo "backlight"
+    else
+        echo "ddc"
+    fi
+}
 
-if [ -z "$delta" ]; then
-    echo "Usage: $0 <variation>"
-    echo "Exemple: $0 -5"
-    exit 1
-fi
+MODE=$(get_mode)
 
-for d in 1 2; do
-    current=$(ddcutil --display "$d" getvcp 10 | grep -oP 'current value =\s*\K\d+')
+case "$1" in
 
+displays)
+    if [ "$MODE" = "backlight" ]; then
+        echo 1
+    else
+        ddcutil detect | grep "Display" | wc -l
+    fi
+;;
+
+get)
+    if [ "$MODE" = "backlight" ]; then
+        brightnessctl -m | awk -F, '{gsub("%","",$4); print $4}'
+    else
+        ddcutil getvcp 10 | grep -oP 'current value =\s*\K\d+' | head -1
+    fi
+;;
+
+get-display)
+    ddcutil --display "$2" getvcp 10 \
+    | grep -oP 'current value =\s*\K\d+'
+;;
+
+set)
+    if [ "$MODE" = "backlight" ]; then
+        brightnessctl set "$2%"
+    else
+        for d in $(seq 1 $(ddcutil detect | grep Display | wc -l)); do
+            ddcutil --display "$d" setvcp 10 "$2"
+        done
+    fi
+;;
+
+set-percentage)
+    if [ "$MODE" = "backlight" ]; then
+        brightnessctl set "$2%"
+    else
+        for d in $(seq 1 $(ddcutil detect | grep Display | wc -l)); do
+            current=$(ddcutil --display "$d" getvcp 10 | grep -oP 'current value =\s*\K\d+')
+            new=$((current + $2))
+
+            [ "$new" -gt 100 ] && new=100
+            [ "$new" -lt 0 ] && new=0
+
+            ddcutil --display "$d" setvcp 10 "$new"
+        done
+    fi
+;;
+
+set-display)
+    ddcutil --display "$2" setvcp 10 "$3"
+;;
+
+set-display-percentage)
+    delta="$3"
+    current=$(ddcutil --display "$2" getvcp 10 | grep -oP 'current value =\s*\K\d+')
     new=$((current + delta))
 
     [ "$new" -gt 100 ] && new=100
     [ "$new" -lt 0 ] && new=0
 
-    ddcutil --display "$d" setvcp 10 "$new"
-done
+    ddcutil --display "$2" setvcp 10 "$new"
+
+esac
